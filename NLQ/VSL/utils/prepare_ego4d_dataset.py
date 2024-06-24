ea66f465d34513067f13540667e0c96a7651200d
lorenzofeliziani <165919413+lorenzofeliziani@users.noreply.github.com>
2024-06-24 19:49:06 +0200

    Update prepare_ego4d_dataset.py

--- NLQ/VSL/utils/prepare_ego4d_dataset.py
+++ NLQ/VSL/utils/prepare_ego4d_dataset.py
@@ -109,6 +109,7 @@ def convert_ego4d_dataset(args):
     os.makedirs(args["clip_feature_save_path"], exist_ok=True)
     progress_bar = tqdm.tqdm(all_clip_video_map.items(), desc="Extracting features")
     for clip_uid, (video_uid, start_sec, end_sec) in progress_bar:
+        print(video_uid)
         feature_path = os.path.join(args["video_feature_read_path"], f"{video_uid}.pt")
         feature = torch.load(feature_path)
 
