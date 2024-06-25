5ec406a79d90d644eac8f5df73c392de81849738
lorenzofeliziani <165919413+lorenzofeliziani@users.noreply.github.com>
2024-06-25 11:52:22 +0200

    Update runner_utils_net.py

--- NLQ/VSL/utils/runner_utils_net.py
+++ NLQ/VSL/utils/runner_utils_net.py
@@ -114,6 +114,7 @@ def eval_test(
                 new_datum = {
                     "clip_uid": record["vid"],
                     "query_idx": int(record["query_idx"]),
+                    "annotation_uid": record["annotation_uid"],
                     "predicted_times": copy.deepcopy(timewindow_predictions),
                 }
                 predictions.append(new_datum)
